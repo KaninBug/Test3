69d0298215660ade27fb9d05b9d84f7c179d2ae6
Jao07Nxne <66050193@kmitl.ac.th>
2024-09-14 01:12:52 +0700

    first commit2

---
+++ calculation.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class calculation {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("input : ");
+        float a = sc.nextFloat();
+        System.out.print("year : ");
+        int y = sc.nextInt();
+        for(int i = 0; i < y; i++) {
+            a += a * 2;
+        }
+        sc.close();
+    }
+}
